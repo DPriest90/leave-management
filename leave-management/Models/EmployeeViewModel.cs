5647f33444e94ad416beb34fdaa7d78e60b75d1d
Stark <Stark@MY-DESKTOP>
2021-09-16 12:24:22 +0100

    Add project files.

---
+++ leave-management/Models/EmployeeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class EmployeeViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+        public int TaxId { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime DateJoined { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ leave-management/Models/LeaveHistoryViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class LeaveHistoryViewModel
+    {     
+        public int Id { get; set; }
+
+        public EmployeeViewModel RequestingEmployee { get; set; }
+
+        public string RequestingEmployeeId { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        public DetailsLeaveTypeViewModel LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public DateTime DateRequested { get; set; }
+
+        public DateTime DateActioned { get; set; }
+
+        public bool? Approved { get; set; }
+
+        public EmployeeViewModel ApprovedBy { get; set; }
+        
+        public string ApprovedById { get; set; }
+
+        /* Lists will allow us to populate a drop down box or some other control to allow us to select from a list */
+        public IEnumerable<SelectListItem> LeaveTypes { get; set; }
+    }
+}
